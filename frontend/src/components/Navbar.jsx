c8986acafac05a85116f002a7182a89ed9f07c9f
Nicolas <nico.rata26@gmail.com>
2026-08-25 11:49:12 +0200

    verif pour navbar connexion/deconnexion

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
 import './Navbar.css';
+import { useAuth } from "../hooks/useAuth";
 
 function Navbar()
 {
+	const { isAuthen, logout } = useAuth();
 	return (
 		<nav className="navbar">
 			<div className="navbar-logo">Task Manager</div>
 			<div className="navbar-links">
 				<Link to="/projects">Projets </Link>
-				<Link to="/login"> Se connecter</Link>
+				{ isAuthen ?
+					(
+						<button onClick={logout}> Se deconnecter</button>
+					)
+					:
+					(
+						<Link to="/login"> Se connecter</Link>
+					)
+				}
 			</div>
 		</nav>
 	);
